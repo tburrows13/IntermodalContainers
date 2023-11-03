c7de0540a28ad74eb83d70db29c1bfbc975fbf16
Tom Burrows <tomburrows13@icloud.com>
2023-11-03 11:51:17 +0000

    Move subgroup generation to data-final-fixes

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -1,3 +1,4 @@
 require("prototypes.public")
 require("prototypes.container-gen")
 require("prototypes.inventory-sizes")
+require("prototypes.container-subgroups")
---
+++ prototypes/container-subgroups.lua
@@ -0,0 +1,26 @@
+local IC = require "prototypes.shared"
+-- Create item subgroups
+for name, subgroup in pairs(data.raw["item-subgroup"]) do
+  if subgroup.group ~= IC.MOD_PREFIX .. "containers" and subgroup.group ~= IC.LOAD_PREFIX .. "container" and subgroup.group ~= IC.UNLOAD_PREFIX .. "container" then
+    data:extend{
+      {
+        type  = "item-subgroup",
+        name  = IC.MOD_PREFIX .. name,
+        group = IC.MOD_PREFIX .. "containers",
+        order = "a[container]-" .. (subgroup.order or "unordered"),
+      },
+      {
+        type  = "item-subgroup",
+        name  = IC.LOAD_PREFIX .. name,
+        group = IC.LOAD_PREFIX .. "container",
+        order = "a[load]-" .. (subgroup.order or "unordered"),
+      },
+      {
+        type  = "item-subgroup",
+        name  = IC.UNLOAD_PREFIX .. name,
+        group = IC.UNLOAD_PREFIX .. "container",
+        order = "b[unload]-" .. (subgroup.order or "unordered"),
+      }
+    }
+  end
+end
